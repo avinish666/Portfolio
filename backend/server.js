e627949ccb31b4999e6d39bd66d1a0567118eb6a
Avinish Kumar <avi77sub@gmail.com>
2026-08-07 16:08:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
-const morgan = require("morgan");
+const morgan = require("morgan")
 require("dotenv").config();
 
 const messageRoutes = require("./routes/messageRoutes");
