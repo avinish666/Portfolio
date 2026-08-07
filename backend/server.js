99e323e9e418a170e2df37cbb64eb052d910c11a
Avinish Kumar <avi77sub@gmail.com>
2026-08-07 18:30:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,6 @@ mongoose
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", function () {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
